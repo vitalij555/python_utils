4c6c41b7857b7a31089cea9f14ba2cfabe03e80e
Vitalij Gortovskij <vitalij555@gmail.com>
2023-11-01 17:04:34 +0200

    Fix in logging initialization logics

--- logging/logging_init.py
+++ logging/logging_init.py
@@ -1,12 +1,20 @@
 import json
-import logging
+
+import os.path
 from pathlib import Path
 
+import importlib
+
+# Dynamically import the original logging module
+original_logging = importlib.import_module('logging')
 
 
 def load_logging_config(logging_file: Path):
+    print(f"Logging config file is {os.path.abspath(logging_file)}")
     with open(logging_file, 'r') as config_file:
         config = json.load(config_file)
 
-    logging.config.dictConfig(config['logging'])
+    original_logging.config.dictConfig(config['logging'])
+    logger = original_logging.getLogger(__name__)
 
+    return logger
