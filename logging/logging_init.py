c6a8665ec7e4a1a596a645e4577a6f6daa65523d
Vitalij Gortovskij <vitalij555@gmail.com>
2023-11-01 17:16:50 +0200

    Fix logging name sading (which is done on purpose here)

--- logging/logging_init.py
+++ logging/logging_init.py
@@ -7,14 +7,15 @@ import importlib
 
 # Dynamically import the original logging module
 original_logging = importlib.import_module('logging')
-
+original_logging_config = importlib.import_module('logging.config')
 
 def load_logging_config(logging_file: Path):
+    config = None
     print(f"Logging config file is {os.path.abspath(logging_file)}")
     with open(logging_file, 'r') as config_file:
         config = json.load(config_file)
 
-    original_logging.config.dictConfig(config['logging'])
+    original_logging_config.dictConfig(config['logging'])
     logger = original_logging.getLogger(__name__)
 
     return logger
