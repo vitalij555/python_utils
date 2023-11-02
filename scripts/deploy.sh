b01a3e925264a8b90f2be0afd32995b6e61337c2
Vitalij Gortovskij <vitalij555@gmail.com>
2023-11-02 11:38:23 +0200

    Adding option support for deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -22,11 +22,16 @@ function increment_version() {
 
 
 function deploy_to_registry() {
+    local VERSION_FILE="./version"
     local increment_type=$1  # major or minor
-    local version_file="../../version"
     local current_version
-    current_version=$(<"$version_file")
 
+    if [[ ! -f "${VERSION_FILE}" ]]; then
+      echo "Error: version file does not exist. Currently looking for it here: ${VERSION_FILE}" >&2
+      return 1
+    fi
+
+    current_version=$(<"$version_file")
 
     local new_version
     new_version=$(increment_version "$current_version" "$increment_type")
@@ -43,10 +48,41 @@ function deploy_to_registry() {
     git push origin master --tags
 }
 
+#private
+function show_help() {
+    echo "Usage: $0 [--help] [-i increment_type]"
+    echo "Options:"
+    echo "   -i    Type of version increment (major or minor)"
+    echo "   --help, -h    Show help"
+}
 
+while :; do
+    case "$1" in
+    -h|--help)
+        show_help
+        exit 0
+        ;;
+    -i)
+        if [ "$2" ]; then
+            increment_type=$2
+            shift
+        else
+            echo "Error: -i requires a non-empty option argument."
+            show_help
+            exit 1
+        fi
+        ;;
+    *)
+        break
+        ;;
+    esac
+    shift
+done
 
-# Usage:
-# ./deploy.sh major
-# or
-# ./deploy.sh minor
-#deploy_to_registry "$1"
+if [ -z "$increment_type" ]; then
+    echo "Error: Increment type is required"
+    show_help
+    exit 1
+fi
+
+deploy_to_registry "$increment_type"
